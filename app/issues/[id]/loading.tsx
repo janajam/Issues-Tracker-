fa22857d9a2a87b6430b5daef75d59cf3f4080f2
Jana Jammoul <jammouljana182@gmil.com>
2026-03-27 17:38:01 +0300

    Addtional loading skkeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,11 +1,20 @@
+import { Box, Card, Flex } from '@radix-ui/themes'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
-import React from 'react'
 
 const IssueDetailsLoading = () => {
   return (
-    <div>
-      Loading ....
-    </div>
+    <Box className='max-w-xl'>
+      <Skeleton />
+      <Flex className='space-x-5' my={'2'}>
+        <Skeleton width={'5rem'} />
+        <Skeleton width={'5rem'} />
+      </Flex>
+      <Card className='prose' mt={'4'}>
+        <Skeleton count={5} />
+      </Card>
+    </Box>
   )
 }
 
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@radix-ui/themes'
+import React from 'react'
+
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+
+const NewIssueLoading = () => {
+  return (
+    <Box className='max-w-xl'>
+        <Skeleton />
+        <Skeleton height={'20rem'}/>
+    </Box>
+  )
+}
+
+export default NewIssueLoading
