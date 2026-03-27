9ad948a58f45cdc8ef533376e2f523149cd6e6ac
Jana Jammoul <jammouljana182@gmil.com>
2026-03-27 19:52:20 +0300

    edit the link component

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -1,16 +1,19 @@
+
 import NextLink from 'next/link'
-import { Link as RadixLink} from '@radix-ui/themes'
-interface Props{
-    href:string,
-    children:string
+import { Link as RadixLink } from '@radix-ui/themes'
+
+interface Props {
+  href: string
+  children: string
 }
 
-const Link = ({href,children}:Props) => {
+const Link = ({ href, children }: Props) => {
   return (
-      <NextLink href={href} passHref legacyBehavior>
-        <RadixLink>{children}</RadixLink>
+    <RadixLink asChild>
+      <NextLink href={href}>
+        {children}
       </NextLink>
-    
+    </RadixLink>
   )
 }
 
