bd7e2e17731130844bfaaa1505f0f3bb78756fd8
Jana Jammoul <jammouljana182@gmil.com>
2026-05-31 15:22:55 +0300

    building pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Pagination from "./components/Pagination";
 
 
 export default function Home() {
   return (
-    <div>hello word</div>
+    <Pagination itemCount={100} pageSize={10} currentPage={10}/>
   );
 }
 
